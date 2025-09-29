0a68d027e9c91e29c886693631a19069e5b80c42
sonogodo <adrexpert@gmail.com>
2025-09-29 20:11:16 -0300

    por favor funciona

--- api/index.py
+++ api/index.py
@@ -13,6 +13,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 # =================================================================
 from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 import json
 
 # =================================================================
@@ -26,6 +27,13 @@ import json
 
 # Inicializa a aplicação FastAPI
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # para produção restrinja para seu domínio
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("api/match_vaga")
 async def match_vaga(descricao: str = Form(...)):
